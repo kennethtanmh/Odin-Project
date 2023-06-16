ecd5eede0b5b4ed1f10900b70f037ff0af197a83
kennethtanmh <kennethtanmh99@gmail.com>
2023-06-16 10:34:34 +0800

    Completed the mock up of the caculator

---
+++ Project: Calculator/index.js
@@ -0,0 +1,26 @@
+const add = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
+
+let firstNumber = 3;
+let operator = "+";
+let secondNumber = 5;
+
+const operate = (operator, a, b) => {
+  switch (operator) {
+    case "+":
+      return add(a, b);
+    case "-":
+      return subtract(a, b);
+    case "*":
+      return multiply(a, b);
+    case "/":
+      return divide(a, b);
+    default:
+      throw new Error("Invalid");
+  }
+};
+
+const test = operate(operator, firstNumber, secondNumber);
+console.log(test);
